d0dc2e77889e5e83bee25345aaae1fb2584b3b35
Aayush Gupta <02guptag@example.com>
2026-04-27 02:49:18 +0530

    Adding subscription billing period dates

--- src/subscriptions/models.py
+++ src/subscriptions/models.py
@@ -149,7 +149,34 @@ class UserSubscription(models.Model):
                 on_delete = models.SET_NULL, null=True, blank=True)
     stripe_id = models.CharField(max_length=120, null=True, blank=True)
     active = models.BooleanField(default=True)
+    original_period_start = models.DateTimeField(auto_now=False, 
+                                                auto_now_add=False, blank=True, null=True)
     user_cancel = models.BooleanField(default=False)
+    current_period_start = models.DateTimeField(auto_now=False, 
+                                                auto_now_add=False, blank=True, null=True)
+    current_period_end = models.DateTimeField(auto_now=False, 
+                                                auto_now_add=False, blank=True, null=True)
+    
+    @property
+    def billing_cycle_anchor(self):
+        """
+        https://docs.stripe.com/payments/checkout/billing-cycle
+        billing_cycle_anchor is a timestamp that Stripe uses to determine when the billing cycle starts and ends.
+        We can use the current_period_end as the billing_cycle_anchor, so that when the subscription renews,
+        it will renew based on the current_period_end.
+                
+        """
+        if not self.current_period_end:
+            return None
+        return int(self.current_period_end.timestamp())
+    
+    
+    def save(self, *args, **kwargs):
+        if (self.original_period_start is None and
+            self.current_period_start is not None):
+            self.original_period_start = self.current_period_start
+             
+        super().save(*args, **kwargs)
 
 def user_sub_post_save(sender, instance, created, **kwargs):
     user_sub_instance = instance
